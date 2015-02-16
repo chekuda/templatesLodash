8560d049dada5ed171b3872c4c86c80f3a0a29aa
chekuda <joselchecaexpo@gmail.com>
2015-02-16 15:59:06 +0000

    functionGenPF
    Adding a function to swap between generic and PF template

--- js.js
+++ js.js
@@ -1,5 +1,39 @@
-window.onload = function() {
+document.onclick = function() {
+	
 }
+	//function to choice Gen o PF template
+	window.onload= function (){
+		var radios = document.getElementsByName("type");
+		for(var i = 0;i < radios.length;i++){
+			radios[i].onclick = function(){
+				var val = this.value
+				//1 = Gen
+				//2 = PF
+				if (val == 2)
+					{
+						//path images
+						$("#pahtGen").css("width","145");
+						$("#pathPF").css({"visibility":"visible", "display":"inline-block"});
+						
+						//text 
+						$("#genTex").css("width","145");
+						$("#Textpf").css({"visibility":"visible", "display":"inline-block"});
+						
+					}
+				else
+					{
+						//path images
+						$("#pahtGen").css("width","310");
+						$("#pathPF").css({"visibility":"hidden", "display":"none"});
+						
+						//text 
+						$("#genTex").css("width","310");
+						$("#Textpf").css({"visibility":"hidden", "display":"none"});
+					}
+				};
+		}
+	}
+	
 
 
     function standardResponsive1()
@@ -8,7 +42,7 @@ window.onload = function() {
 		var merchantName = document.getElementById('Merchant').value;
 		var links = document.getElementById('links').value;
 		var imagesGen = document.getElementById('pahtGen').value;
-		//var imagesPF = document.getElementById('pahtPF').value;
+		var imagesPF = document.getElementById('pahtPF').value;
 		//var colorBg =  document.getElementById('background').value;
 		var textGen = document.getElementById('genTex').value;
 		//var textPF = document.getElementById('genTex').value;
@@ -18,16 +52,22 @@ window.onload = function() {
 		var socialLink3 = document.getElementById('social3').value;
                 
                  var selection = document.querySelector("input[name='standard']:checked").value;
-                var selectionKey = {
+                var selectionKeyGen = {
+                    '1': 'standard1Gen.html',
+                    '2': 'standard2Gen.html',
+                    '3': 'standard3Gen.html',
+                    '4': 'dach.html'};
+		
+				var selectionKeyPF = {
                     '1': 'standard1Gen.html',
                     '2': 'standard2Gen.html',
                     '3': 'standard3Gen.html',
                     '4': 'dach.html'};
                     
-		//llamo al servidor donde tengo el archivo
+		//llamo al servidor donde tengo el archivo Gen
 		$.ajax({
                     //esta URL me cojera la url donde tengo el archivo ./standard.html
-                    url: selectionKey[selection],
+                    url: './Gen/'+ selectionKeyGen[selection],
                     //si la conexion esta bien hecha entro en success
                     success: function(data)
                     {
@@ -35,6 +75,24 @@ window.onload = function() {
 			var compiled = _.template(data);
 			text = compiled({HOMEPAGELINK: links, MERCHANTNAME: merchantName, ImageKey: imagesGen, TEXTCONTENT: textGen,DISCLAIMER: disclaimer,SOCIALLINK1: socialLink1,SOCIALLINK2:socialLink2,SOCIALLINK3:socialLink3});
 			document.getElementById('GenText').innerHTML = text;
+                    }, 
+                    error: function() {
+			console.log(arguments);
+			document.getElementById('GenText').innerHTML = 'IDIOTA!'
+                    }
+                });
+		
+		//llamo al servidor donde tengo el archivo PF
+		$.ajax({
+                    //esta URL me cojera la url donde tengo el archivo ./standard.html
+                    url: selectionKeyPF[selection],
+                    //si la conexion esta bien hecha entro en success
+                    success: function(data)
+                    {
+                    //primero paso el el archivo a compiled y luego lo sustituyo por los datos
+			var compiled = _.template(data);
+			text = compiled({HOMEPAGELINK: links, MERCHANTNAME: merchantName, ImageKey: imagesGen, TEXTCONTENT: textGen,DISCLAIMER: disclaimer,SOCIALLINK1: socialLink1,SOCIALLINK2:socialLink2,SOCIALLINK3:socialLink3});
+			document.getElementById('PFText').innerHTML = text;
                     }, 
                     error: function() {
 			console.log(arguments);
