7c9f1dc13eb8a3f801ce021793ecb8f9e0af4ad7
chekuda <joselchecaexpo@gmail.com>
2015-06-28 12:25:22 +0800

    Added function previewHTML
    I've created this function to preview the HTML code paste on the GenBox and PFBox

--- js.js
+++ js.js
@@ -7,62 +7,134 @@ document.onclick = function() {
     {
         //*****************************  select the PF or Gen required   **************************** 
                 
-                $("[name='standard']").on('click',clickedPF);
-                $("#checkGen").on('click',clickedPF);
-                $("#checkPF").on('click',clickedPF);
-                function clickedPF()
-                {
-                    var standardSelected = document.querySelector("input[name='standard']:checked").value;
-                    if(document.querySelector('#checkPF:checked'))
-                    {
-                        
-                        //path images
-                        $("#pahtGen").css("width","145");
-                        $("#pathPF").css({"visibility":"visible", "display":"inline-block"});
+		$("[name='standard']").on('click',clickedPF);
+		$("#checkGen").on('click',clickedPF);
+		$("#checkPF").on('click',clickedPF);
+		function clickedPF()
+		{
+			var standardSelected = document.querySelector("input[name='standard']:checked").value;
+			if(document.querySelector('#checkPF:checked'))
+			{
 
-                        //text 
-                        $("#genTex").css("width","145");
-                        $("#Textpf").css({"visibility":"visible", "display":"inline-block"});
+				//path images
+				$("#pahtGen").css("width","145");
+				$("#pathPF").css({"visibility":"visible", "display":"inline-block"});
 
-                        //secondText
-                        $("#divFormPf2").css({"visibility":"visible", "display":"inline-block"});
-                        
+				//text 
+				$("#genTex").css("width","145");
+				$("#Textpf").css({"visibility":"visible", "display":"inline-block"});
 
-                        console.log('PF[' + standardSelected + ']' + ' ' + 'Gen[' + standardSelected + ']');
-                            
-                               
-                    }
-                    else if(document.querySelector('#checkGen:checked'))
-                    {
-                        if(standardSelected == 1)//standard 1Gen
-                            {
-                                console.log('gen + std1');
-                                //path images
-                                $("#pahtGen").css("width","310");
-                                $("#pathPF").css({"visibility":"hidden", "display":"none"});
+				//secondText
+				$("#divFormPf2").css({"visibility":"visible", "display":"inline-block"});
 
-                                //text 
-                                $("#genTex").css("width","310");
-                                $("#Textpf").css({"visibility":"hidden", "display":"none"});
 
-                                //secondText
-                                $("#divFormPf2").css({"visibility":"hidden", "display":"none"});
-                                
-                            }
-                        console.log('PF[' + standardSelected + ']' + ' ' + 'Gen[' + standardSelected + ']');
-                    }
-                };
+				console.log('PF[' + standardSelected + ']' + ' ' + 'Gen[' + standardSelected + ']');
+
+
+			}
+			else if(document.querySelector('#checkGen:checked'))
+			{
+				if(standardSelected == 1)//standard 1Gen
+				{
+					console.log('gen + std1');
+					//path images
+					$("#pahtGen").css("width","310");
+					$("#pathPF").css({"visibility":"hidden", "display":"none"});
+
+					//text 
+					$("#genTex").css("width","310");
+					$("#Textpf").css({"visibility":"hidden", "display":"none"});
+
+					//secondText
+					$("#divFormPf2").css({"visibility":"hidden", "display":"none"});
+
+				}
+				console.log('PF[' + standardSelected + ']' + ' ' + 'Gen[' + standardSelected + ']');
+			}
+	};
           //*****************************  select the PF or Gen required  ****************************  
           //
           //
           //
           //*****************************  Load the Template    **************************** 
 
-        $(".myButton.preview").on('click',standardResponsive1);
+        
 
-        function standardResponsive1()
+	//*****************************  Preview Template   **************************** 
+	
+	$(".myButton.preview").on('click',previewHTML);
+	
+	function previewHTML(){
+			
+			//check if I have the iframe already
+			var iframeN = document.getElementById("iframe-content");
+			var iframe;
+			var iframeDoc;
+			var codeText;
+			//catch the value of the button clicked
+			var typeTemplate = $(this).text();
+
+			//if not, I will create the iframe and add it on the id="iframe-body"
+			if(!iframeN)
+			{
+				iframe = document.createElement("iframe");
+				//iframe.id("iframe-content");
+				iframe.setAttribute("id","iframe-content");
+
+				//paste the new iframe on my id created already
+				$("#iframe-body").append(iframe);
+				
+			}
+		
+			//***calling the function getContentBox()
+			var codeText = getContentBox(typeTemplate);
+			
+			
+				
+			//Replacing my iframe content for the HTML created
+			var newIframe = document.getElementById('iframe-content');
+			newIframe.contentWindow.document.open()
+			newIframe.contentWindow.document.write(codeText);
+			$("#iframe-content").css({'display':'block','z-index': '2'});
+			$('div#overlay').css({'display':'block'});
+	
+	    
+	};
+		
+	//*****Get the text from the Box
+	function getContentBox(type){
+		
+		if(type == 'Preview Gen')
+				{
+					var codeText = $('#GenText').text();
+				}
+		else
+				{
+					var codeText = $('#PFText').text();
+
+				}	
+		return codeText;
+		
+	}
+		
+	//Closing the Overlay
+	//
+	$('#overlay').on('click',closeOverlay);
+		
+		function closeOverlay()
+		{
+			document.getElementById('iframe-content').remove();
+			$('div#overlay').css({'display':'none'});
+			
+		}
+	
+	
+	//*****************************  Displaying the code    ****************************
+	
+	$(".myButton.coding").on('click',displayCode);
+	
+        function displayCode()
         {
-                console.log('hola');
 
         
                 var merchantName = document.getElementById('Merchant').value;
@@ -128,9 +200,8 @@ document.onclick = function() {
                             }
                         });
 			
-			
-			
-			
+
+
                         
         };      
 		
@@ -140,6 +211,7 @@ document.onclick = function() {
 		//
 		//
 		$(".myButton.download").on('click',downloadFile);
+		
 		
 		function downloadFile (){
 			var merchantName = document.getElementById('Merchant').value;
